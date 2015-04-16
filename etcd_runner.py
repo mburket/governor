82b97234bb75c27ae92f4e52823c632612c2757f
Yoni Ryabinski <cigan1@gmail.com>
2015-04-16 11:07:40 -0400

    Working on dynamoc cmd to launch etcd.

--- etcd_runner.py
+++ etcd_runner.py
@@ -7,11 +7,15 @@ from helpers.etcd import Etcd
 import local_lib
 
 # main
+# vars
+discovery = "https://discovery.etcd.io/f71aca0ff87c3e7f9dcd9838266b7631"
+data_dir = "/var/lib/etcd/default.etcd/"
 etcd_ip = local_lib.ec2_ip()
+etcd_name = ocal_lib.ec2_name()
 #print etcd_ip
 
 # test that etcd exists
-cmd = "/bin/etcd -bind-addr=0.0.0.0:4001 -addr=" + etcd_ip + ":4001"
+cmd = "/bin/etcd -bind-addr=0.0.0.0:4001 -addr=" + etcd_ip + ":4001 -discovery=" + discovery + " -name=" + etcd_name
 print cmd
 try:
 	pass
--- local_lib.py
+++ local_lib.py
@@ -10,3 +10,10 @@ def ec2_ip():
         return urllib2.urlopen(url).read()
     except (urllib2.HTTPError, urllib2.URLError) as e:
         raise e
+
+def ec2_name():
+    url = "http://169.254.169.254/latest/meta-data/hostname"
+    try:
+        return urllib2.urlopen(url).read()
+    except (urllib2.HTTPError, urllib2.URLError) as e:
+        raise e        
