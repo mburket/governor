247cc27b1fb428b8b3e0c78c17eee22cc4b91214
Yoni Ryabinski <cigan1@gmail.com>
2015-04-16 15:28:38 -0400

    Update value.

--- etcd_runner.py
+++ etcd_runner.py
@@ -14,22 +14,20 @@ ip = local_lib.ec2_ip()
 hostname = local_lib.ec2_name()
 config = { "scope": "batman", "ttl": 45, "host": "127.0.0.1:4001" }
 etcd = Etcd(config)
+host = ip.":4001"
 
 # main
 cmd = [ "/bin/etcd", "-bind-addr=0.0.0.0:4001", "-addr=" + ip + ":4001", "-discovery=" + discovery, "-name=" + hostname, "-peer-addr=" + ip + ":7001", "-peer-bind-addr=0.0.0.0:7001", "-peer-heartbeat-interval=100", "-peer-election-timeout=500" ]
 print cmd
 try:
-	p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-	# out, err = p.communicate()
-	# if len(err) > 0:
-	# 	raise Exception(err)
+	subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 except Exception, e:
 	raise e
 
 while True:
 	try:
 		# update the etcd leader key
-		etcd.put_client_path("/etcd_leader", { "host": ip, "ttl": config["ttl"] })
+		etcd.put_client_path("/etcd_leader", { "value": host, "ttl": config["ttl"] })
 		print "update leader key"		
 	except Exception, e:
 		pass
