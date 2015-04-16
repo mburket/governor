94bb8d2983453609516143d714f86510ac90f61a
Yoni Ryabinski <cigan1@gmail.com>
2015-04-16 11:03:12 -0400

    Genrating the dynamic command.

--- etcd_runner.py
+++ etcd_runner.py
@@ -8,10 +8,11 @@ import local_lib
 
 # main
 etcd_ip = local_lib.ec2_ip()
-print etcd_ip
+#print etcd_ip
 
 # test that etcd exists
-cmd = "/bin/etcd"
+cmd = "/bin/etcd -bind-addr=0.0.0.0:4001 -addr=" + etcd_ip + ":4001"
+print cmd
 try:
 	pass
 except Exception, e:
