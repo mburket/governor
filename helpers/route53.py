c7892a167cb6f8249a986d29b1b70ff17bd2304c
Yoni Ryabinski <cigan1@gmail.com>
2015-04-21 10:37:39 -0400

    Started working on updating route53.

--- helpers/route53.py
+++ helpers/route53.py
@@ -1,6 +1,10 @@
-
+import logging
+from boto import route53
 
 class rt53:
-
 	def __init__(self, config):
-		pass
+		self.region = config["region"]
+		self.conn = route53.connect_to_region(self.region)
+		self.zone = self.conn.get_zone(config["zone"])
+
+	
