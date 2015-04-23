65680ce154625448229fc7048a862896a963e35c
Yoni Ryabinski <cigan1@gmail.com>
2015-04-23 16:09:00 -0400

    Changed name to confirm to cert.

--- helpers/rt53.py
+++ helpers/rt53.py
@@ -9,10 +9,11 @@ class Rt53:
 		conn = route53.connect_to_region(config["region"])
 		self.zone = conn.get_zone(config["zone"])
 		self.domain = config["zone"]
+		self.stack = config["stack"]
 		self.our_ip = config["our_ip"]
 
 	def update(self):
-		key = "database.prod.%s" % (self.domain)
+		key = "database-%s.%s" % (self.stack, self.domain)
 		try:
 			self.zone.update_a(key, self.our_ip, ttl = self.ttl)
 		except AttributeError:
