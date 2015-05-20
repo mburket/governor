b8b08453c6999193f0ba110dfdda53e66e6de214
Yoni Ryabinski <cigan1@gmail.com>
2015-05-20 11:51:51 -0400

    Moved DNS ttl into config.

--- helpers/rt53.py
+++ helpers/rt53.py
@@ -3,13 +3,13 @@ from boto import route53
 # docs: http://boto.readthedocs.org/en/latest/ref/route53.html#module-boto.route53.record
 
 class Rt53:
-	def __init__(self, config):
-		self.ttl = 15
+	def __init__(self, config):		
 		conn = route53.connect_to_region(config["region"])
 		self.zone = conn.get_zone(config["zone"])
 		self.domain = config["zone"]
 		self.stack = config["stack"]
 		self.our_ip = config["our_ip"]
+		self.ttl = config["ttl"]
 
 	def update(self):
 		key = "database-%s.%s" % (self.stack, self.domain)
