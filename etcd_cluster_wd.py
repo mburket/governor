bb21898c249ae3270c94b52c68f2f1afc102fecd
Yoni Ryabinski <cigan1@gmail.com>
2015-06-03 15:49:30 -0400

    Almost done.

--- etcd_cluster_wd.py
+++ etcd_cluster_wd.py
@@ -2,18 +2,24 @@
 
 import subprocess, os
 
+stop_cmd = [ '/bin/systemctl', 'stop', 'etcd' ]
+start_cmd = [ '/bin/systemctl', 'start', 'etcd' ]
 
 try:
 	cmd = [ '/bin/etcdctl', 'cluster-health' ]
 	p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 	out, err = p.communicate()
-	print out
 	lines = out.split(os.linesep)
 
 	for l in lines:
 		find = l.find('cluster')
 		if find == 0:
 			print l
+			args = l.split(' ')
+			if not args[2] == 'healthy':
+				print 'need to restart'
+			else:
+				print 'healthy'
 
 except Exception, e:
 	raise e
