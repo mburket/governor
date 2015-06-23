fe51835d30ac23a74b602b27b8c2d4e487cb09a4
Yoni Ryabinski <cigan1@gmail.com>
2015-06-23 14:49:11 -0400

    Commented out some code.

--- governor.py
+++ governor.py
@@ -87,7 +87,7 @@ while True:
     if postgresql.is_leader():
         for node in etcd.get_client_path("/members?recursive=true")["node"]["nodes"]:
             member = node["key"].split('/')[-1]
-            if member != postgresql.name:
-                postgresql.query("DO LANGUAGE plpgsql $$DECLARE somevar VARCHAR; BEGIN SELECT slot_name INTO somevar FROM pg_replication_slots WHERE slot_name = '%(slot)s' LIMIT 1; IF NOT FOUND THEN PERFORM pg_create_physical_replication_slot('%(slot)s'); END IF; END$$;" % {"slot": member})
+            # if member != postgresql.name:
+            #     postgresql.query("DO LANGUAGE plpgsql $$DECLARE somevar VARCHAR; BEGIN SELECT slot_name INTO somevar FROM pg_replication_slots WHERE slot_name = '%(slot)s' LIMIT 1; IF NOT FOUND THEN PERFORM pg_create_physical_replication_slot('%(slot)s'); END IF; END$$;" % {"slot": member})
 
     time.sleep(config["loop_wait"])
