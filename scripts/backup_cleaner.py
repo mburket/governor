1046c358e3ab7a448da14c40fe752716b79a56d8
Yoni Ryabinski <cigan1@gmail.com>
2015-08-31 13:45:21 -0400

    Working on failed backup cleanup script.

---
+++ scripts/backup_cleaner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import subprocess
+
+
+cmd = [ "barman", "list-backup", "all" ]
+try:
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out, err = p.communicate()
+    if len(err) > 0:
+        raise Exception(str(err))
+    print out
+except Exception as e:
+    print str(e)
+    raise e
